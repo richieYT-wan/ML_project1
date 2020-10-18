5c0ce82b3aaaf39067aeb4b0e873bad4f93a7e8c
Richie Wan <richie.wan@epfl.ch>
2020-10-18 19:11:42 +0200

    Created train_tune
    +added test.csv to git.ignore
    * now working on this branch instead of local repo

---
+++ scripts/.ipynb_checkpoints/train_tune-checkpoint.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from implementations import *
+"""Implements functions needed for training, hyper-parameters tuning, cross-validation, as well as demo functions to look at output"""
+
+
+def build_k_indices(y, k_fold, seed):
+    """build k indices for k-fold."""
+    num_row = y.shape[0]
+    interval = int(num_row / k_fold)
+    np.random.seed(seed)
+    indices = np.random.permutation(num_row)
+    k_indices = [indices[k * interval: (k + 1) * interval] for k in range(k_fold)]
+    return np.array(k_indices)
