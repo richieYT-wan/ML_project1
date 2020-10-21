e4e7b5549a43e6424355da7fe7b5262605fb69fa
Richie Wan <richie.wan@epfl.ch>
2020-10-21 10:59:16 +0200

    moved fcts, fixed histograms

--- scripts/proj1_helpers.py
+++ scripts/proj1_helpers.py
@@ -3,6 +3,8 @@
 import csv
 import numpy as np
 from costs import sigmoid
+import matplotlib.pyplot as plt
+
 
 def load_csv_data(data_path, sub_sample=False):
     """Loads data and returns y (class labels), tX (features) and ids (event ids)"""
@@ -58,4 +60,28 @@ def create_csv_submission(ids, y_pred, name):
         writer.writeheader()
         for r1, r2 in zip(ids, y_pred):
             writer.writerow({'Id':int(r1),'Prediction':int(r2)})
+
+            
+def histogram_clusters(tx0,tx1,tx2,tx3,header="../data/train.csv"):
+    """
+    Function to visualize the distribution of all features after having been clustered.
+    
+    """
+    fig, axes = plt.subplots(10,3,figsize=(8,20),sharex=False,sharey =False,)
+    a = axes.ravel()
+    
+    for idx,ax in enumerate(a[:]):        
+        
+        ax.hist(tx0[:,idx],bins=500,stacked=True, histtype='stepfilled',alpha=0.5, label = "Cluster 0", color="cornflowerblue")
+        ax.hist(tx1[:,idx],bins=500,stacked=True, histtype='stepfilled',alpha=0.5, label = "Cluster 1", color = "red")
+        ax.hist(tx2[:,idx],bins=500,stacked=True, histtype='stepfilled',alpha=0.5, label = "Cluster 2", color="cyan")
+        ax.hist(tx3[:,idx],bins=500,stacked=True, histtype='stepfilled',alpha=0.5, label = "Cluster 3", color ="purple")
+        #ax.set_adjustable(self, adjustable, share=False)
+        handles, labels = ax.get_legend_handles_labels()
+        ax.legend(loc='best',fontsize="x-small")
+        ax.autoscale()
+
+        ax.set_title(header[idx+2])
+    plt.tight_layout() 
+    plt.show()
 
